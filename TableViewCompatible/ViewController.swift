b2b2630f2e0b93106d726347679bfb34ece6681b
Fredrik Nannestad <fredrik.nannestad@jayway.com>
2016-10-21 10:35:21 +0200

    Initial Commit

---
+++ TableViewCompatible/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewCompatible
+//
+//  Created by Fredrik Nannestad on 21/10/2016.
+//  Copyright © 2016 Fredrik Nannestad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
